f0512fc39dfad7aee2a3d83b421661b1bc8cec15
Mohammad Ali Yektaie <yektaie112358@gmail.com>
2021-12-18 02:42:18 -0800

    Added the analyzer, but the image is rotated 90 degrees.

--- app/src/main/java/com/test/corevisionandroidx/core/capture/CameraCaptureFragment.java
+++ app/src/main/java/com/test/corevisionandroidx/core/capture/CameraCaptureFragment.java
@@ -1,12 +1,19 @@
 package com.test.corevisionandroidx.core.capture;
 
+import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Size;
 import android.view.LayoutInflater;
 import android.view.Surface;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +21,8 @@ import androidx.camera.core.AspectRatio;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraInfoUnavailableException;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.core.impl.ImageOutputConfig;
 import androidx.camera.lifecycle.ProcessCameraProvider;
@@ -25,6 +34,10 @@ import androidx.fragment.app.Fragment;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.test.corevisionandroidx.R;
 
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -35,8 +48,11 @@ public class CameraCaptureFragment extends Fragment {
     private ICameraCaptureFragmentListener listener = null;
     private int cameraMode = AspectRatio.RATIO_4_3;
     private Preview preview = null;
+    private ImageAnalysis imageAnalysis = null;
     private Camera camera = null;
     private PreviewView cameraPreviewSurface = null;
+    private ImageView cameraProcessedSurface = null;
+    private Bitmap bitmapBuffer = null;
 
     public CameraCaptureFragment() {
         super(R.layout.camera_capture_fragment);
@@ -48,6 +64,7 @@ public class CameraCaptureFragment extends Fragment {
 
         cameraExecutor = Executors.newSingleThreadExecutor();
         cameraPreviewSurface = view.findViewById(R.id.camera_preview_surface);
+        cameraProcessedSurface = view.findViewById(R.id.camera_processed_surface);
         setViewAspectRatio();
         setupCamera();
     }
@@ -55,18 +72,22 @@ public class CameraCaptureFragment extends Fragment {
     private void setViewAspectRatio() {
         cameraMode = this.getArguments() != null ? this.getArguments().getInt("aspect_ratio", AspectRatio.RATIO_4_3) : AspectRatio.RATIO_4_3;
 
-        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) cameraPreviewSurface.getLayoutParams();
+        ConstraintLayout.LayoutParams previewLayoutParams = (ConstraintLayout.LayoutParams) cameraPreviewSurface.getLayoutParams();
+        ConstraintLayout.LayoutParams processedLayoutParams = (ConstraintLayout.LayoutParams) cameraProcessedSurface.getLayoutParams();
         if (cameraMode == AspectRatio.RATIO_4_3) {
-            layoutParams.dimensionRatio = "3:4";
+            previewLayoutParams.dimensionRatio = "3:4";
+            processedLayoutParams.dimensionRatio = "3:4";
         } else if (cameraMode == AspectRatio.RATIO_16_9) {
-            layoutParams.dimensionRatio = "9:16";
+            previewLayoutParams.dimensionRatio = "9:16";
+            processedLayoutParams.dimensionRatio = "9:16";
         } else {
             throw new RuntimeException();
         }
     }
 
     private void setupCamera() {
-        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext());
+        Context context = requireContext();
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(context);
         cameraProviderFuture.addListener(() -> {
             try {
                 cameraProvider = cameraProviderFuture.get();
@@ -91,12 +112,73 @@ public class CameraCaptureFragment extends Fragment {
                 .setTargetRotation(Surface.ROTATION_0)
                 .build();
 
+        imageAnalysis = setupImageAnalysis();
+
         cameraProvider.unbindAll();
         try {
-            camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview);
+            camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalysis);
             preview.setSurfaceProvider(cameraPreviewSurface.getSurfaceProvider());
         } catch (Exception ex) {
             listener.onError(ex, "Unable to bind the camera use cases life cycle to fragment.");
+        }
+    }
+
+    private ImageAnalysis setupImageAnalysis() {
+//        ImageAnalysisCon imageAnalysisConfig = new ImageAnalysisConfig.Builder()
+//                .setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
+//                .setCallbackHandler(new Handler(analyzerThread.getLooper()))
+//                .setImageQueueDepth(1).build();
+
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setTargetResolution(getTargetResolution(cameraMode))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                .build();
+
+        imageAnalysis.setAnalyzer(cameraExecutor, (image) -> {
+            processCameraFrame(image);
+        });
+
+        return imageAnalysis;
+    }
+
+    private void processCameraFrame(ImageProxy image) {
+        if (bitmapBuffer == null) {
+            bitmapBuffer = Bitmap.createBitmap(image.getWidth(), image.getHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        bitmapBuffer.copyPixelsFromBuffer(image.getPlanes()[0].getBuffer());
+        getActivity().runOnUiThread(() -> {
+            cameraProcessedSurface.setImageBitmap(bitmapBuffer);
+        });
+        image.close();
+        //        Utils.
+//        final Bitmap bitmap = image.getImage();
+//
+//
+//
+//        if(bitmap==null)
+//            return;
+//
+//        Mat mat = new Mat();
+//        Utils.bitmapToMat(bitmap, mat);
+//
+//
+//        Imgproc.cvtColor(mat, mat, currentImageType);
+//        Utils.matToBitmap(mat, bitmap);
+//        runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                ivBitmap.setImageBitmap(bitmap);
+//            }
+//        });
+    }
+
+    private Size getTargetResolution(int cameraMode) {
+        if (cameraMode == AspectRatio.RATIO_4_3) {
+            return new Size(720, 960);
+        } else {
+            return new Size(720, 1280);
         }
     }
 
@@ -111,11 +193,6 @@ public class CameraCaptureFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-    }
-
-    @Override
-    public void onConfigurationChanged(@NonNull Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
     }
 
     @Override
